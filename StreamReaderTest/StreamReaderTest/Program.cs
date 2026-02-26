1b3cfa3a45e68d140330e028bb7ff189f0dc54fb
Sinomar Melo <synnomar@hotmail.com>
2026-02-26 13:23:31 -0300

    modo diferente de instanciar o StreamReader

--- StreamReaderTest/StreamReaderTest/Program.cs
+++ StreamReaderTest/StreamReaderTest/Program.cs
@@ -9,27 +9,26 @@ namespace MyApplication
         {
             string path = @"c:\temp\file.txt";
 
-            FileStream fs = null;
-            StreamReader sr = null;
-
             try
             {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
-
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
             }
-            catch (IOException e) {
-                Console.WriteLine("An error accurred");
-                Console.WriteLine(e.Message);
-            }
-            finally
+            catch (IOException e)
             {
-                if (fs != null) sr.Close();
-                if (fs != null) fs.Close();
-             }
 
+                Console.WriteLine("An error occurred:");
+                Console.WriteLine(e.Message);
+            }                  
         }
     }
 }
