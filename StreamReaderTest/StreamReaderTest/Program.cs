e87b7a92cc5b9077b2065b8d59bf58d7015688a0
Sinomar Melo <synnomar@hotmail.com>
2026-02-26 13:05:29 -0300

    StreamReader

---
+++ StreamReaderTest/StreamReaderTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file.txt";
+
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null) sr.Close();
+                if (fs != null) fs.Close();
+             }
+
+        }
+    }
+}
