ab3d99c21c6d2f69ba4d5afe5b0d0c406f5a0066
Sinomar Melo <synnomar@hotmail.com>
2026-02-26 14:45:52 -0300

    Stream Write

---
+++ StreamWriterTest/StreamWriterTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace MyApplicativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
