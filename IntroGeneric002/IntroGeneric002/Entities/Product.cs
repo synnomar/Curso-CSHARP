9b364c3e3c6ff8d864ebaa8bd026ab4acefba91f
Sinomar Melo <synnomar@hotmail.com>
2026-03-03 16:30:45 -0300

    ComparTo

---
+++ IntroGeneric002/IntroGeneric002/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace IntroGeneric002.Entities
+{
+    class Product : IComparable 
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name 
+                + ", " 
+                + Price.ToString("F2", CultureInfo.InvariantCulture);                     
+                ;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentNullException("Comparing erro: argument is nor Product");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ IntroGeneric002/IntroGeneric002/Program.cs
@@ -0,0 +1,35 @@
+﻿using IntroGeneric002.Entities;
+using IntroGeneric002.Services;
+using System;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter Number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("MAX: ");
+            Console.WriteLine(max);
+        }
+    }
+}
